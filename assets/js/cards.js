956044ffe2abe95e27040e60b1f0b16160723a83
ajabadi <ajabadi@ucdavis.edu>
2023-11-05 21:06:43 -0800

    search-navbar-update

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -93,36 +93,48 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
 })
-
+//Variable for Google Books API base URL
 var gBooksURL = "https://www.googleapis.com/books/v1/volumes?q=";
+//Variable for Google Books API key
 var gBooksAPIKey = "AIzaSyAB-DMWo1SEDPqiD8Ihs-wgBnfsUTn9DRo";
-
+//Selects the form element from the html
 var cardsSearchForm = document.getElementById('search-form');
+//Selects the text input field where the user will enter their unput
 var cardsUserInput = document.getElementById('book-input-field'); 
-
+//Adds an event listener of submit to the cards search form
 cardsSearchForm.addEventListener('submit', function (e) {
+    //Gets the value entered by the user in the input field at the cards html
     var inputVal = cardsUserInput.value;
+    //Prevents the page from refreshing
     e.preventDefault();
+     //Calls the searchBooks function with a argument of the users input value
     searchBooks(inputVal);
 });
-
+//Function to search for books using Google Books API based on user input
 function searchBooks(input) {
+    //Creates the full URL by concating user input and the API key to the base URL
     var fullUrl = gBooksURL + input + "&key=" + gBooksAPIKey;
-    
+     //Makes an http request to Google Books
     fetch(fullUrl)
     .then(function(resp) {
+       //If the response is not an OK code a new Error will be thrown and at the end of the response chain, skipping any code between
         if (!resp.ok) {
             throw new Error(`HTTP error! Status: ${resp.status}`);
         }
+         //Returns the response in json format
         return resp.json();
     })
+   //Parsed json data from the above returned promise will be stored in data
     .then(function(data) {
         if (data === undefined || data.items === undefined) {
             throw new Error('Data is undefined');
         }
+       //Sets our searched data items into an array of objects in local storage for us to be able to grab by the key of'books' later
         localStorage.setItem('books', JSON.stringify(data.items));
+       //When the user searches a book we are directed to the cards.html which is connected to the cards.html
         window.location.href = "cards.html";
     })
+     //Catches our thrown error and logs the error in the console
     .catch(function(err) {
         console.error(err, "Could not fetch data");
     });
